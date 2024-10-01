960fc3a20947a6282ff065e98db63f3ef0bdfc36
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-10-01 22:47:40 +0100

    test: remove duplicate asserts that verify tx vsize

--- test/functional/feature_framework_miniwallet.py
+++ test/functional/feature_framework_miniwallet.py
@@ -8,9 +8,6 @@ import string
 
 from test_framework.blocktools import COINBASE_MATURITY
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import (
-    assert_equal,
-)
 from test_framework.wallet import (
     MiniWallet,
     MiniWalletMode,
@@ -30,7 +27,6 @@ class FeatureFrameworkMiniWalletTest(BitcoinTestFramework):
             for target_vsize in [250, 500, 1250, 2500, 5000, 12500, 25000, 50000, 1000000,
                                  248, 501, 1085, 3343, 5805, 12289, 25509, 55855,  999998]:
                 tx = wallet.create_self_transfer(utxo_to_spend=utxo, target_vsize=target_vsize)["tx"]
-                assert_equal(tx.get_vsize(), target_vsize)
 
     def test_wallet_tagging(self):
         """Verify that tagged wallet instances are able to send funds."""
