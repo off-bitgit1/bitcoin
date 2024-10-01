9b1bdabe9c56a2a19967d25ad155ef544c7b9e7e
Sjors Provoost <sjors@sprovoost.nl>
2024-10-01 09:47:50 +0200

    depends: update libmultiprocess library
    This update brings in the following changes:
    chaincodelabs/libmultiprocess#111: doc: Add internal design section
    chaincodelabs/libmultiprocess#113 Add missing include to util.h

--- depends/packages/native_libmultiprocess.mk
+++ depends/packages/native_libmultiprocess.mk
@@ -1,8 +1,8 @@
 package=native_libmultiprocess
-$(package)_version=015e95f7ebaa47619a213a19801e7fffafc56864
+$(package)_version=6929c40913ce2bbee58754f2984ddd7ec97b44ea
 $(package)_download_path=https://github.com/chaincodelabs/libmultiprocess/archive
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=4b1266b121337f3f6f37e1863fba91c1a5ee9ad126bcffc6fe6b9ca47ad050a1
+$(package)_sha256_hash=e0aab3166ca902e7b255b4e1766c71c5e99170534ae412136e4133d4f85745d7
 $(package)_dependencies=native_capnp
 
 define $(package)_config_cmds
