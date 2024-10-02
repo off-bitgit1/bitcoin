65c0a981261a3ac10eff9661bb5f23f6a244d725
Andre Alves <andremralves@gmail.com>
2024-10-02 10:59:50 -0300

    contrib: skip missing binaries in gen-manpages
    Instead of stopping the execution of gen-manpages.py when a binary
    is not found, continue generating manpages for the available binaries
    and skip the missing ones.

--- contrib/devtools/gen-manpages.py
+++ contrib/devtools/gen-manpages.py
@@ -33,13 +33,14 @@ mandir = os.getenv('MANDIR', os.path.join(topdir, 'doc/man'))
 # Verify that all the required binaries are usable, and extract copyright
 # message in a first pass.
 versions = []
-for relpath in BINARIES:
+for relpath in BINARIES[:]:
     abspath = os.path.join(builddir, relpath)
     try:
         r = subprocess.run([abspath, "--version"], stdout=subprocess.PIPE, check=True, text=True)
     except IOError:
-        print(f'{abspath} not found or not an executable', file=sys.stderr)
-        sys.exit(1)
+        print(f'{abspath} not found or not an executable. Skipping...', file=sys.stderr)
+        BINARIES.remove(relpath)
+        continue
     # take first line (which must contain version)
     verstr = r.stdout.splitlines()[0]
     # last word of line is the actual version e.g. v22.99.0-5c6b3d5b3508
